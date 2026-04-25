d08d8e398154649610d7a9158e1bbeb35e9106f6
Nikhil Goyal <nikhilgoyal24680@gmail.com>
2026-04-25 14:34:36 +0530

    Added webapp properly

---
+++ frontend/webapp/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button class="start" @click="getStarted">Get Started</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    getStarted() {
+      
+    }
+  }
+}
+</script>
+
+<style>
+.start {
+  color: white;
+  height: 20px;
+  background-color: rgb(168, 50, 50);
+  border-radius: 5px;
+  border: none;
+  size: 100px;
+}
+</style>
